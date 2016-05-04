b4eb456164e2d088f59358bc406624962a7b5d26
Ruben <rbntejeda@hotmail.com>
2016-05-04 09:24:53 -0300

    Creacion de script de movimiento

---
+++ Assets/motionCharacter.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class motionCharacter : MonoBehaviour {
+	public GameObject Camera=null,Personaje=null;
+	public float velocidadMovimiento = 2f,velocidadCamara=2f;
+
+	void Start () {
+		if (Camera == null)
+			GameObject.FindGameObjectWithTag ("MainCamera");
+		if (Personaje == null)
+			Personaje = this.gameObject;
+	}
+
+	void FixedUpdate () {
+		Personaje.gameObject.transform.position += new Vector3 (Input.GetAxis ("Horizontal"), 0, Input.GetAxis ("Vertical")) * velocidadMovimiento * Time.deltaTime;
+		Camera.gameObject.transform.position = Input.mousePosition;
+	}
+}
